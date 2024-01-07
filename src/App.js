1b3d0b00d6bf92ef6f00247611a4d9b9a5ab96e2
umar-developer-01 <umarkhandev9@gmail.com>
2024-01-07 20:48:40 +0530

    Changes in payments

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import Login from "./Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
 import Checkout from "./Checkout";
+import Payment from "./Payment";
 import Home from "./Home";
+import Orders from "./Orders";
 import { auth,onAuthStateChanged } from "./firebase.js";
 import './App.css';
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+const promise = loadStripe(
+  "pk_test_51HPvU9DFg5koCdLGJJbNo60QAU99BejacsvnKvT8xnCu1wFLCuQP3WBArscK3RvSQmSIB3N0Pbsc7TtbQiJ1vaOi00X9sIbazL"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -17,8 +24,7 @@ useEffect(() => {
   // will only run once when the app component loads...
 
   onAuthStateChanged(auth,(authUser) => {
-    console.log("THE USER IS >>> ", authUser);
-
+    
     if (authUser) {
       // the user just logged in / the user was logged in
       dispatch({
@@ -41,6 +47,22 @@ useEffect(() => {
     <div className="app">
      <Router>
        <Routes>
+         <Route exact path="/orders" element={ 
+             <>
+              <Header />
+              <Orders />
+            </>
+            } 
+          />
+             <Route exact path="/payment" element={ 
+             <>
+              <Header />
+              <Elements stripe={promise}>
+                <Payment />
+              </Elements>
+            </>
+            } 
+          />
          <Route exact path="/checkout" element={ 
              <>
               <Header/>
